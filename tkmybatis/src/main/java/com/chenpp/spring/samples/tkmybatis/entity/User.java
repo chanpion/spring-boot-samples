9e88da98e22690d859dd90e1f3e9a328701bafb0
陈朋朋 <pengpeng.chen@tongdun.net>
2023-04-26 15:17:48 +0800

    feat: add tkmybatis demo

--- tkmybatis/src/main/java/com/chenpp/spring/samples/tkmybatis/entity/User.java
+++ tkmybatis/src/main/java/com/chenpp/spring/samples/tkmybatis/entity/User.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
 /**
  * @author April.Chen
@@ -18,4 +19,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "JDBC")
     private Long id;
+    private String username;
+    private Date gmtCreate;
+    private Date gmtModify;
 }
